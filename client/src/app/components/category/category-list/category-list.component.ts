11587fe0892d0522483f170fff7d8caa7ecf0ecf
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-14 21:22:05 +0200

    Pagination functionality for categories all page

--- client/src/app/components/category/category-list/category-list.component.ts
+++ client/src/app/components/category/category-list/category-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CategoryService } from 'src/app/core/services/category.service';
 import Category from '../../shared/models/category';
-import { Observable, Subscription } from 'rxjs';
+
+import { globalConstants } from 'src/app/common/global-constants';
+import getPage from 'src/app/common/paginator';
 
 @Component({
   selector: 'app-category-list',
@@ -9,9 +11,22 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent {
-  categories$: Observable<Category[]>
+  page: number = globalConstants.pagination.defaultPage;
+  collectionSize: number;
+  private itemsPerPage: number;
+  categories: Category[] = [];
+  allCategories: Category[] = [];
 
-  constructor(private categoryService: CategoryService) { 
-    this.categories$ = categoryService.all();
+  constructor(private categoryService: CategoryService) {
+    this.itemsPerPage = globalConstants.pagination.itemsPerPage;
+    this.categoryService.all().subscribe(data => {
+      this.allCategories = data;
+      this.getCategoriesPerPage(this.page);
+    })
+  }
+
+  public getCategoriesPerPage(page: number): void {
+    debugger;
+    this.categories = getPage<Category>(this.allCategories, page, this.itemsPerPage);
   }
 }
--- client/src/app/components/product/product-info/product-info.component.ts
+++ client/src/app/components/product/product-info/product-info.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import Product from '../../shared/models/product.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProductDeleteModalComponent } from '../product-delete-modal/product-delete-modal.component';
-import { modelConstants } from 'src/app/common/model-constants';
 import { ProductService } from 'src/app/core/services/product.service';
-import { privateDecrypt } from 'crypto';
 import { Router } from '@angular/router';
 
 @Component({
